073e0ea84f3c0b720dd58e62d24c68f82ac468d6
Dawid Wojda <93827901+Dawid-Wojda@users.noreply.github.com>
2022-03-10 20:17:31 +0100

    Add files via upload

---
+++ LAB1_Dawid_Wojda.py
@@ -0,0 +1,77 @@
+import math
+
+#zad1
+# z1 ="napis1"
+# z2 = "napis2"
+# z3 = 5
+# z4 = 10
+# z5 = 4.5
+# z6 = 7.2
+# z7 = 2+5j
+# z8 = 6+3j
+
+# print(z1)
+# print(z2)
+# print(z3)
+# print(z4)
+# print(z5)
+# print(z6)
+# print(z7)
+# print(z8)
+
+#zad2
+# z9 = 5
+# z10 = 2
+# print(z9 + z10)
+# print(z9 - z10)
+# print(z9 * z10)
+# print(z9 ** z10)
+# print(z9 / z10)
+# print(z9 // z10)
+# print(z9 % z10)
+
+#zad3
+# z11 = 5
+# print(z11)
+# z11 += 5
+# print(z11)
+# z11 -= 5
+# print(z11)
+# z11 *= 5
+# print(z11)
+# z11 /= 5
+# print(z11)
+# z11 **= 5
+# print(z11)
+# z11 %= 2
+# print(z11)
+
+#zad4
+
+# print(math.e**10)
+# print(math.pow(math.log(5+math.pow(math.sin(8),2),math.e),6))
+# print(math.floor(3.55))
+# print(math.ceil(4.80))
+
+#zad5
+
+# zIMIE = "DAWID"
+# zNAZWISKO = "WOJDA"
+
+#print(zIMIE.capitalize() + " " + zNAZWISKO.capitalize())
+
+#zad6
+
+# zPiosenka = "Pszczółka Maja sobie lata (oh oh oh)"
+# print(zPiosenka.count("oh"))
+
+#zad7
+
+# zLancuch = "Nazywam się Dawid Wojda"
+# print(zLancuch[0] + zLancuch[-1])
+# print(zLancuch[0] + zLancuch[len(zLancuch)-1])
+
+#zad8
+
+# zslowa = zPiosenka.split(' ')
+# print(zslowa)
