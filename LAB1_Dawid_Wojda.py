9ddff5193147add17a1e25ff84417101a023e270
Dawid Wojda <93827901+Dawid-Wojda@users.noreply.github.com>
2022-03-25 09:56:29 +0100

    Update LAB1_Dawid_Wojda.py

--- LAB1_Dawid_Wojda.py
+++ LAB1_Dawid_Wojda.py
@@ -49,7 +49,7 @@ import math
 #zad4
 
 # print(math.e**10)
-# print(math.pow(math.log(5+math.pow(math.sin(8),2),math.e),6))
+# print(pow(math.log(5+pow(math.sin(8), 2)), 1/6))
 # print(math.floor(3.55))
 # print(math.ceil(4.80))
 
